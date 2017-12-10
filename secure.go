9fe66e33f783bb59c8d1557032f62f5d1a0e1851
Blixt <me@blixt.nyc>
2017-12-10 10:35:48 +0100

    Add SecureHostEnforcer

--- secure.go
+++ secure.go
@@ -15,3 +15,15 @@ func SecureEnforcer(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+
+// SecureHostEnforcer ensures HTTPS in environments that provide the
+// X-Forwarded-Proto header and also ensures the host is correct.
+func SecureHostEnforcer(host string, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Host != host || r.Header.Get("X-Forwarded-Proto") == "http" {
+			http.Redirect(w, r, fmt.Sprintf("https://%s%s", host, r.RequestURI), http.StatusFound)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
